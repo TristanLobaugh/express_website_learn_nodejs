d64dbebb12ca2809424901a91c3781fa728a32c5
TristanLobaugh <ttrader_atl@yahoo.com>
2017-03-29 23:27:14 -0400

    added nodemailer module so page can send email

--- app.js
+++ app.js
@@ -20,5 +20,42 @@ app.get('/about', (req, res, next) => {
 	res.render('about');
 });
 
+app.get('/contact', (req, res, next) => {
+	res.render('contact');
+});
+
+app.post('/contact/send', (req, res, next) => {
+	const transporter = nodemailer.createTransport({
+		service: 'Gmail',
+		auth: {
+			user: 'myEmail@gmail.com',
+			pass: 'myPassword'
+		}
+	});
+
+	const mailOptions = {
+		from: 'Tristan Lobaugh <myEmail@gmail.com>',
+		to: 'anotherEmail@gmail.com',
+		subject: 'Website Submission',
+		text: `You have a submission with the following details... Name: ${req.body.name} | Email: ${req.body.email} | Message: ${req.body.message}`,
+		html: `<p>You have a submission with the following details...</p>
+					<ul>
+						<li>Name: ${req.body.name}</li>
+						<li>Email: ${req.body.email}</li>
+						<li>Message: ${req.body.message}</li>
+					</ul>`
+	};
+
+	transporter.sendMail(mailOptions, (err, info) => {
+		if (err) {
+			console.log(err);
+			res.redirect('/');
+		} else {
+			console.log(`Message sent! ${info.response}`);
+			res.redirect('/');
+		}
+	});
+});
+
 app.listen(3000);
 console.log('Server is running on port 3000...');
